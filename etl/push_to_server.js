ddecce07674ef5a1361e58b39c79a192c8584112
monkeywidget <monkeywidget@users.noreply.github.com>
2018-02-03 20:48:36 -0800

    file buffer reading

---
+++ etl/push_to_server.js
@@ -0,0 +1,12 @@
+var csvFileName = process.argv[2]
+
+console.log('I\'ll load in the file named "' + csvFileName + '"')
+
+var fs = require('fs');
+var path = require('path');
+
+var BUFFER = bufferFile(csvFileName);
+
+function bufferFile(relPath) {
+    return fs.readFileSync(path.join(__dirname, relPath)); // zzzz....
+}
